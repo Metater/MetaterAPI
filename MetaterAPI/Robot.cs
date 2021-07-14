6c8bfdafade69e4fcc4f6aabf42a16674580c047
Connor <61102529+Metater@users.noreply.github.com>
2021-07-14 13:18:11 -0400

    Make robot stuff save to db

--- MetaterAPI/Robot.cs
+++ MetaterAPI/Robot.cs
@@ -5,25 +5,31 @@ using Grapevine.Shared;
 using System;
 using System.Text;
 using MetaterAPI.Utils;
+using System.IO;
 
 namespace MetaterAPI
 {
     [RestResource]
     public class Robot
     {
-        int leftSpeed = 90;
-        int rightSpeed = 90;
+        string robotSpeedsDBPath => Directory.GetCurrentDirectory() + @"\robotSpeeds.db";
+
 
         [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/getrobot")]
         public IHttpContext GetRobotSpeeds(IHttpContext context)
         {
-            context.Response.SendResponse(leftSpeed + ":" + rightSpeed);
+            (int, int) robotSpeeds = GetRobotSpeeds();
+
+            context.Response.SendResponse(robotSpeeds.Item1 + ":" + robotSpeeds.Item2);
             return context;
         }
 
         [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/setrobot")]
         public IHttpContext SetRobotSpeeds(IHttpContext context)
         {
+            int leftSpeed = 90;
+            int rightSpeed = 90;
+
             bool valid = false;
 
             if (QueryString.ContainsQueryString(context, "left") && QueryString.ContainsQueryString(context, "right"))
@@ -42,9 +48,27 @@ namespace MetaterAPI
             }
             else
             {
+                SetRobotSpeeds(leftSpeed, rightSpeed);
                 context.Response.SendResponse($"Left Speed: {leftSpeed}\nRight Speed: {rightSpeed}");
             }
             return context;
+        }
+
+        private (int, int) GetRobotSpeeds()
+        {
+            if (!File.Exists(robotSpeedsDBPath))
+            {
+                File.WriteAllText(robotSpeedsDBPath, "90\n90");
+            }
+
+            string robotSpeeds = File.ReadAllText(robotSpeedsDBPath);
+            string[] robotSpeedsSplit = robotSpeeds.Split('\n');
+            return (int.Parse(robotSpeedsSplit[0]), int.Parse(robotSpeedsSplit[1]));
+        }
+
+        private void SetRobotSpeeds(int left, int right)
+        {
+            File.WriteAllText(robotSpeedsDBPath, left + "\n" + right);
         }
     }
 }
